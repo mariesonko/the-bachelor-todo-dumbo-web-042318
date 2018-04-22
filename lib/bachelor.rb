fe4409743e3c95ebeb66a334e001ee83418053c9
Marie Sonko <mariesonko62@gmail.com>
2018-04-22 18:59:06 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -40,9 +40,8 @@ def get_occupation(data, hometown)
         return contest_name [occupation].first
       end
     end
-end 
-  end
 end
+  end
 
 def get_average_age_for_season(data, season)
   # code here

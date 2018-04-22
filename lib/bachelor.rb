3906f57e1fb4751872167c9bc247cc371cb4c2bc
Marie Sonko <mariesonko62@gmail.com>
2018-04-22 19:13:02 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -34,12 +34,14 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  data[season].each do |contestants|
-      if contestants["hometown"] == hometown
-        return contestants ["occupation"]
+  data.each do |season_nbr, contestants|
+    contestants.each do |contest_name|
+      if contest_name["hometown"] == hometown
+        return contest_name ["occupation"]
         break
       end
     end
+end
   end
 
 def get_average_age_for_season(data, season)

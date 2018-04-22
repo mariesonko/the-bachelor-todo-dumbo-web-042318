ae24c6db710b674f88aba4ac2d7bbaacf3b01c47
Marie Sonko <mariesonko62@gmail.com>
2018-04-22 18:39:07 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -14,7 +14,7 @@ end
 def get_contestant_name(data, occupation)
   data.each do |occup_type, type|
     if occup_type == occupation
-      type.each_value {|type_info| puts "#{type_info}"|}
+      type.each_value {|type_info| puts type_info|}
   end
   end
 end

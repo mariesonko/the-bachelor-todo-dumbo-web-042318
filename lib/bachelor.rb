2b065ca5f09fc2750d96be839e97c3a59f570772
Marie Sonko <mariesonko62@gmail.com>
2018-04-22 18:16:12 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,12 @@
 def get_first_name_of_season_winner(data, season)
+data.each do |season_number, contestants|
+if season_number == season
 
+  contestants.each_value do |contes_name| puts "#{contes_name}"
+  if contes_name["status"] == "Winner"
+  end
+end
+end 
 end
 
 def get_contestant_name(data, occupation)
